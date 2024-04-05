984ce9f18e37e368920d098d1d94f2f737f61c00
Maxou305 <maxime.chazard@le-campus-numerique.fr>
2024-04-05 13:27:58 +0200

    Comment routes added

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Comment;
-use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreCommentRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use App\Http\Requests\UpdateCommentRequest;
 
 class CommentController extends Controller
@@ -14,7 +14,40 @@ class CommentController extends Controller
      */
     public function index()
     {
-        //
+        return Comment::all();
+    }
+
+    public function getById($id)
+    {
+        $comment = Comment::find($id);
+        if ($comment === null) {
+            return response()->json(['message' => 'Comment not found'], 404);
+        }
+        return $comment;
+    }
+
+    public function getByUserId($id)
+    {
+        $comment = Comment::where('user_id', $id)->get();
+        if ($comment === null) {
+            return response()->json(['message' => 'Comment not found'], 404);
+        }
+        if ($comment->count() === 1) {
+            return $comment[0];
+        }
+        return $comment;
+    }
+
+    public function getByProductId($id)
+    {
+        $comment = Comment::where('product_id', $id)->get();
+        if ($comment === null) {
+            return response()->json(['message' => 'Comment not found'], 404);
+        }
+        if ($comment->count() === 1) {
+            return $comment[0];
+        }
+        return $comment;
     }
 
     /**
@@ -28,9 +61,16 @@ class CommentController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreCommentRequest $request)
+    public function store(Request $request)
     {
-        //
+        $request->validate([
+            'content' => 'required',
+            'user_id' => 'required',
+            'product_id' => 'required',
+        ]);
+        $comment = Comment::create($request->all());
+        $comment->save();
+        return response()->json($comment, 201);
     }
 
     /**
@@ -52,16 +92,26 @@ class CommentController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateCommentRequest $request, Comment $comment)
+    public function update(Request $request, $id): JsonResponse
     {
-        //
+        $comment = Comment::find($id);
+        if ($comment === null) {
+            return response()->json(['message' => 'Comment not found'], 404);
+        }
+        $comment->update(['content' => $request->get('content')]);
+        return response()->json($comment, 201);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Comment $comment)
+    public function destroy($id): JsonResponse
     {
-        //
+        $comment = Comment::find($id);
+        if ($comment === null) {
+            return response()->json(['message' => 'Comment not found'], 404);
+        }
+        $comment->delete();
+        return response()->json(['message' => 'Comment deleted']);
     }
 }
