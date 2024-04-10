e4ccba94f30addab3f7d3ff247b3e1747aa45353
Maxou305 <maxime.chazard@le-campus-numerique.fr>
2024-04-10 13:58:49 +0200

    uses updated

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Auth\LoginRequest;
-use App\Http\Requests\Auth\MeRequest;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
