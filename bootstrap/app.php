7b253a51d8d748406ac08bef1d0f5c52d33e7ba5
lpzkjdoi <lpzkjdoi@icloud.com>
2024-06-21 16:53:45 +0200

    TODO : CORS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,8 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+//    TODO handle this shit
     ->withMiddleware(function (Middleware $middleware) {
-       // $middleware->statefulApi();
+        $middleware->statefulApi();
 //        $middleware->use(
 //            [CorsMiddleware::class,
 //            HandleCors::class,
